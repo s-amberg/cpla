8d0f6b60ae1c3b66f428c52566092ecb7ee054cc
Hansruedi Patzen <hpatzen@hsr.ch>
2019-04-23 12:51:02 +0200

    connectfour: update sfml build script

--- week10/exercise_libs/SFML/build.sh
+++ week10/exercise_libs/SFML/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE="Debug" -DBUILD_SHARED_LIBS=False && make
+mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE="Debug" -DBUILD_SHARED_LIBS=False -DSFML_USE_STATIC_STD_LIBS=True && make
 
