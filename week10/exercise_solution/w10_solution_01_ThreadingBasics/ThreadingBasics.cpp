ae564e0e762105af9e5765e90aeb53c9e7476aa8
Thomas Corbat <thomas.corbat@hsr.ch>
2019-04-25 12:42:55 +0200

    add thread basics prime counter code

--- week10/exercise_solution/w10_solution_01_ThreadingBasics/ThreadingBasics.cpp
+++ week10/exercise_solution/w10_solution_01_ThreadingBasics/ThreadingBasics.cpp
@@ -1,9 +1,6 @@
 #include <functional>
-
 #include <iostream>
-
 #include <thread>
-
 
 struct IdPrinter {
 	void operator()(std::ostream & out) const {
@@ -15,15 +12,19 @@ void printId(std::ostream & out) {
 	out << std::this_thread::get_id() << '\n';
 }
 
-
-int main() {
+void printIds() {
 	auto idPrintLambda = [](std::ostream & out) {
 		out << std::this_thread::get_id() << '\n';
 	};
-	std::thread tFunctor{IdPrinter{}, std::ref(std::cout)};
-	std::thread tFunction{printId, std::ref(std::cout)};
-	std::thread tLambda{idPrintLambda, std::ref(std::cout)};
+	std::thread tFunctor { IdPrinter { }, std::ref(std::cout) };
+	std::thread tFunction { printId, std::ref(std::cout) };
+	std::thread tLambda { idPrintLambda, std::ref(std::cout) };
 	tLambda.join();
 	tFunction.join();
 	tFunctor.join();
+}
+
+
+int main() {
+	printIds();
 }
