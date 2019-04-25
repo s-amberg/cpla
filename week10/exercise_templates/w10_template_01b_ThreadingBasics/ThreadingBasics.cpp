d27583ba9ef3dc2ac0baafa9b7b19e6149aa0604
Thomas Corbat <tcorbat@hsr.ch>
2019-04-25 12:33:12 +0000

    Update week10/exercise_templates/w10_template_01b_ThreadingBasics/ThreadingBasics.cpp

--- week10/exercise_templates/w10_template_01b_ThreadingBasics/ThreadingBasics.cpp
+++ week10/exercise_templates/w10_template_01b_ThreadingBasics/ThreadingBasics.cpp
@@ -40,7 +40,7 @@ constexpr int numberOfThreads = 1;
 void measure() {
 	auto const startTime = std::chrono::high_resolution_clock::now();
 
-	auto const result = countPrimesParallel(start, end, 4);
+	auto const result = countPrimes(start, end);
 
 	auto const endTime = std::chrono::high_resolution_clock::now();
 	auto const duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
