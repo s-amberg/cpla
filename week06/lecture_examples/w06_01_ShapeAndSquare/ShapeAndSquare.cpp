87f98a7bfaf33e2f3a02568df1dab18b8ea50f97
Thomas Corbat <tcorbat@hsr.ch>
2020-03-16 16:36:22 +0100

    undo change to ShapeAndSquare

--- week06/lecture_examples/w06_01_ShapeAndSquare/ShapeAndSquare.cpp
+++ week06/lecture_examples/w06_01_ShapeAndSquare/ShapeAndSquare.cpp
@@ -15,12 +15,7 @@ struct Square : Shape {
 };
 
 
-struct Empty {};
 
-struct S {
-	int e1;
-	[[no_unique_address]] Empty e2;
-};
 
 int main(int argc, char **argv) {
 	//1 for non-virtual (empty) Shape
@@ -30,8 +25,4 @@ int main(int argc, char **argv) {
 	//4 or 8 for non-virtual Square
 	//8 to 16 for virtual Square
 	std::cout << sizeof(Square) << '\n';
-
-	std::cout << sizeof(S) << '\n';
-	S s{};
-	std::cout << std::hex << "address of e1 0x" << &s.e1 << "\naddress of e2 0x" << &s.e2 << '\n';
 }
