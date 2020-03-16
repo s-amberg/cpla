7c992938c77dc858c76476f2d7e6f90ab3514611
Thomas Corbat <tcorbat@hsr.ch>
2020-03-16 09:41:56 +0100

    week 6 purge

--- week06/exercise_templates/w06_template_02_BoundedBufferWithHeapMemory/src/bounded_buffer_content_suite.h
+++
@@ -1,8 +0,0 @@
-#ifndef BOUNDED_BUFFER_CONTENT_SUITE_H_
-#define BOUNDED_BUFFER_CONTENT_SUITE_H_
-
-#include "cute_suite.h"
-
-extern cute::suite make_suite_bounded_buffer_content_suite();
-
-#endif
--- week06/exercise_templates/w06_template_03_BoundedBufferForNonDefaultConstructibleTypes/src/bounded_buffer_non_default_constructible_element_type_suite.h
+++
@@ -1,9 +0,0 @@
-#ifndef BOUNDED_BUFFER_NON_DEFAULT_CONSTRUCTIBLE_ELEMENT_TYPE_SUITE_H_
-#define BOUNDED_BUFFER_NON_DEFAULT_CONSTRUCTIBLE_ELEMENT_TYPE_SUITE_H_
-
-#include "cute_suite.h"
-
-extern cute::suite make_suite_bounded_buffer_non_default_constructible_element_type_suite();
-
-
-#endif
