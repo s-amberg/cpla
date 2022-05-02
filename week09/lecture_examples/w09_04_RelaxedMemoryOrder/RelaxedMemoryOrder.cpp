b9727076e0f14329558b233d1ce875b0f90a36a3
Felix Morgner <felix.morgner@gmail.com>
2022-05-02 22:26:08 +0200

    week09: update lecture slides

--- week09/lecture_examples/w09_04_RelaxedMemoryOrder/RelaxedMemoryOrder.cpp
+++ week09/lecture_examples/w09_04_RelaxedMemoryOrder/RelaxedMemoryOrder.cpp
@@ -1,74 +1,32 @@
 #include <atomic>
 #include <thread>
-#include <array>
-#include <iostream>
-using namespace std::this_thread;
+#include <cassert>
 
-constexpr size_t max_count { 10 };
-constexpr size_t max_threads { 5 };
-constexpr std::memory_order order = std::memory_order_relaxed; //std::memory_order_seq_cst; //std::memory_order_relaxed
+std::atomic<bool> x{};
+std::atomic<bool> y{};
+std::atomic<int> z{};
 
-// latch
-std::atomic<bool> startflag { };
-// subject to load/stores:
-std::atomic<int> x { };
-std::atomic<int> y { };
-std::atomic<int> z { };
-volatile int mem{0};
-
-struct values_read {
-	int x, y, z;
-};
-
-void waitForStartFlag() {
-	while (!startflag)
-		yield();
+void write_x_then_y()
+{
+	x.store(true, std::memory_order_relaxed);
+	y.store(true, std::memory_order_relaxed);
 }
 
-void increment(std::atomic<int>&x_or_y, std::array<values_read, max_count> &val) {
-	waitForStartFlag();
-	for (size_t i = 0; i < max_count; ++i) {
-		val[i].x = x.load(order);
-		val[i].y = y.load(order);
-		val[i].z = z.load(order);
-		x_or_y.store(i + 1, order);
-		yield();
+void read_y_then_x()
+{
+	while(!y.load(std::memory_order_relaxed)) {
+		// Spin ...
 	}
-}
 
-void read(std::array<values_read, max_count> &val) {
-	waitForStartFlag();
-	for (size_t i = 0; i < max_count; ++i) {
-		val[i].x = x.load(order);
-		val[i].y = y.load(order);
-		val[i].z = z.load(order);
-		yield();
+	if(x.load(std::memory_order_relaxed)) {
+		++z;
 	}
-}
-void print(std::array<values_read, max_count> const &val) {
-	for (auto const &elt : val) {
-		std::cout << "(" << elt.x << "," << elt.y << "," << elt.z << "),";
-	}
-	std::cout << '\n';
 }
 
 int main() {
-	std::array<std::array<values_read, max_count>, max_threads> values { };
-
-	std::thread inc1 { increment, std::ref(x), std::ref(values[0]) };
-	std::thread inc2 { increment, std::ref(y), std::ref(values[1]) };
-	std::thread inc3 { increment, std::ref(z), std::ref(values[2]) };
-	std::thread read1 { read, std::ref(values[3]) };
-	std::thread read2 { read, std::ref(values[4]) };
-
-	startflag = true; // open latch
-	inc1.join();
-	inc2.join();
-	inc3.join();
-	read1.join();
-	read2.join();
-
-	for (auto const &row : values) {
-		print(row);
-	}
+	auto a = std::thread{write_x_then_y};
+	auto b = std::thread{read_y_then_x};
+	a.join();
+	b.join();
+	assert(z.load() != 0);
 }
