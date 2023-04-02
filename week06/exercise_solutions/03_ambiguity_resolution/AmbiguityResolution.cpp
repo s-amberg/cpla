850de94133d05d9d1ce0729c09db1e2e299c4459
tcorbat <thomas.corbat@gmail.com>
2023-04-02 22:55:17 +0200

    Add exercise 3

---
+++ week06/exercise_solutions/03_ambiguity_resolution/AmbiguityResolution.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+#include <string>
+
+struct Date {
+
+  Date(unsigned day, unsigned month, unsigned year) : day{day}, month{month}, year{year}{}
+
+  auto print(std::ostream& os) const -> void {
+    os << day << "/" << month << "/" << year;
+  }
+
+private:
+   unsigned day;
+   unsigned month;
+   unsigned year;
+};
+
+auto operator<<(std::ostream& os, Date const& date) -> std::ostream& {
+  date.print(os);
+  return os;
+}
+
+
+template <typename T>
+concept LeftshiftOutputtable = requires (T const v, std::ostream& os) { {os << v} -> std::same_as<std::ostream&>; };
+
+template <typename T>
+concept Printable = requires (T const v, std::ostream& os) { 
+  v.print(os);
+};
+
+//Possible solution: Add another concept to invert the existing
+template <typename T>
+concept NotLeftshiftOutputtable = !LeftshiftOutputtable<T>;
+
+//Add an additional constraint to the overload
+auto print(Printable auto const& printable) requires NotLeftshiftOutputtable<decltype(printable)> {
+  printable.print(std::cout);
+}
+
+auto print(LeftshiftOutputtable auto const& outputtable) {
+  std::cout << outputtable;
+}
+
+auto printAll(auto const& first, auto const&... rest) -> void {
+  print(first);
+  if constexpr (sizeof...(rest)) {
+    std::cout << ", ";
+    printAll(rest...);
+  }
+}
+
+
+auto main() -> int {
+  using namespace std::string_literals;
+  Date d{4, 4, 2023};
+  printAll(1, 2.0, 3, "four", "five"s, d);
+}
