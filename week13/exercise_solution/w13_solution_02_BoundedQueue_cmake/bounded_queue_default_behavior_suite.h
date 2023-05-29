c02ecdaa1ffa64b0a3a277c8bbdc963a58c6cc41
Felix Morgner <felix.morgner@gmail.com>
2023-05-29 21:18:21 +0200

    week13: update week 13

--- week13/exercise_solution/w13_solution_02_BoundedQueue_cmake/bounded_queue_default_behavior_suite.h
+++
@@ -1,8 +0,0 @@
-#ifndef BOUNDED_QUEUE_DEFAULT_BEHAVIOR_SUITE_H_
-#define BOUNDED_QUEUE_DEFAULT_BEHAVIOR_SUITE_H_
-
-#include "cute_suite.h"
-
-extern cute::suite make_suite_bounded_queue_default_behavior_suite();
-
-#endif
--- week13/exercise_solution/w13_solution_03_BoundedQueue_structure/test/include/bounded_queue_signatures_suite.h
+++
@@ -1,8 +0,0 @@
-#ifndef BOUNDED_QUEUE_SIGNATURES_SUITE_H_
-#define BOUNDED_QUEUE_SIGNATURES_SUITE_H_
-
-#include "cute_suite.h"
-
-extern cute::suite make_suite_bounded_queue_signatures_suite();
-
-#endif
