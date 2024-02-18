7d6cc0944b96977fe5ff3ba112359cfb582977ae
tcorbat <thomas.corbat@gmail.com>
2024-02-18 12:32:59 +0000

    Add Catch2 version of NotOnHeap

--- week04/exercise_templates/01_not_on_heap/NotOnHeapTests.cpp
+++ week04/exercise_templates/01_not_on_heap/NotOnHeapTests.cpp
@@ -1,25 +1,9 @@
 #include "NotOnHeap.hpp"
 
-#include <cute/cute.h>
-#include <cute/cute_runner.h>
-#include <cute/ide_listener.h>
-#include <cute/summary_listener.h>
-
+#include <catch2/catch_test_macros.hpp>
 
 
 // Allocation as local variable
-TEST(test_stack_allocation_works) {
+TEST_CASE("Stack Allocation Works", "NotOnHeap Suite") {
 }
 
-
-auto main(int argc, char const* argv[]) -> int {
-  auto suite = cute::suite{"NotOnHeap Tests",
-                           {
-                               test_stack_allocation_works,
-                           }};
-
-  auto listener = cute::ide_listener<cute::summary_listener<>>{};
-  auto runner = cute::makeRunner(listener, argc, argv);
-
-  return runner(suite) ? EXIT_SUCCESS : EXIT_FAILURE;
-}
