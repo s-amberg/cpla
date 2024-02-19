867c6411278dfc8e3dd363a6f9a202907915aa07
tcorbat <thomas.corbat@gmail.com>
2024-02-19 20:55:53 +0000

    Add Catch2 tests for BoundedBuffer w5

---
+++ week05/exercise_templates/01_bounded_buffer_with_iterators/tests/catch2/BoundedBufferNDCElementTypeSuite.cpp
@@ -0,0 +1,163 @@
+#include "BoundedBuffer.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+
+struct NonDefaultConstructible {
+  NonDefaultConstructible(int) : value{1234} {}
+
+  ~NonDefaultConstructible() {
+    throwIfNotInitialized();
+    nOfDtorCalls++;
+  }
+
+  NonDefaultConstructible(NonDefaultConstructible const& other)
+      : value{other.value} {
+    throwIfNotInitialized();
+    nOfCopyConstructions++;
+  }
+
+  auto operator=(NonDefaultConstructible const& other)
+      -> NonDefaultConstructible& {
+    throwIfNotInitialized();
+    other.throwIfNotInitialized();
+    value = other.value;
+    nOfCopyAssignments++;
+    return *this;
+  }
+
+  auto operator=(NonDefaultConstructible&& other) -> NonDefaultConstructible& {
+    throwIfNotInitialized();
+    other.throwIfNotInitialized();
+    std::swap(value, other.value);
+    nOfMoveAssignments++;
+    return *this;
+  }
+
+  NonDefaultConstructible(NonDefaultConstructible&& other) : value{1234} {
+    other.throwIfNotInitialized();
+    std::swap(value, other.value);
+    nOfMoveConstructions++;
+  }
+
+  static unsigned nOfCopyConstructions;
+  static unsigned nOfCopyAssignments;
+  static unsigned nOfMoveConstructions;
+  static unsigned nOfMoveAssignments;
+  static unsigned nOfDtorCalls;
+
+ private:
+  void throwIfNotInitialized() const {
+    if (value != 1234) {
+      throw std::logic_error{"Operation on NDC with value: " +
+                             std::to_string(value)};
+    }
+  }
+  volatile unsigned value{};
+};
+
+unsigned NonDefaultConstructible::nOfCopyConstructions{0};
+unsigned NonDefaultConstructible::nOfCopyAssignments{0};
+unsigned NonDefaultConstructible::nOfMoveConstructions{0};
+unsigned NonDefaultConstructible::nOfMoveAssignments{0};
+unsigned NonDefaultConstructible::nOfDtorCalls{0};
+
+void resetCounters() {
+  NonDefaultConstructible::nOfCopyConstructions = 0;
+  NonDefaultConstructible::nOfCopyAssignments = 0;
+  NonDefaultConstructible::nOfMoveConstructions = 0;
+  NonDefaultConstructible::nOfMoveAssignments = 0;
+  NonDefaultConstructible::nOfDtorCalls = 0;
+}
+
+TEST_CASE("New buffer of non-default constructible invokes no destructors", "[Non-Default-Constructible Element Suite]") {
+  // resetCounters();
+  // BoundedBuffer<NonDefaultConstructible> buffer{5};
+  // REQUIRE(NonDefaultConstructible::nOfDtorCalls == 0);
+}
+
+TEST_CASE("Element in buffer is destroyed once", "[Non-Default-Constructible Element Suite]") {
+  // {
+  //   BoundedBuffer<NonDefaultConstructible> buffer{5};
+  //   buffer.push(NonDefaultConstructible{23});
+  //   resetCounters();
+  // }
+  // REQUIRE(NonDefaultConstructible::nOfDtorCalls == 1);
+}
+
+TEST_CASE("Element in moved buffer is destroyed once", "[Non-Default-Constructible Element Suite]") {
+  // {
+  //   BoundedBuffer<NonDefaultConstructible> buffer{5};
+  //   buffer.push(NonDefaultConstructible{23});
+  //   resetCounters();
+
+  //   BoundedBuffer<NonDefaultConstructible> moved{std::move(buffer)};
+  // }
+
+  // REQUIRE(NonDefaultConstructible::nOfDtorCalls == 1);
+}
+
+TEST_CASE("Every element in copied buffer is destroyed once", "[Non-Default-Constructible Element Suite]") {
+  // {
+  //   BoundedBuffer<NonDefaultConstructible> buffer{5};
+  //   buffer.push(NonDefaultConstructible{23});
+  //   resetCounters();
+
+  //   BoundedBuffer<NonDefaultConstructible> copy{buffer};
+  // }
+  // REQUIRE(NonDefaultConstructible::nOfDtorCalls == 2);
+}
+
+TEST_CASE("Pop destroys element", "[Non-Default-Constructible Element Suite]") {
+  // BoundedBuffer<NonDefaultConstructible> buffer{5};
+  // buffer.push(NonDefaultConstructible{23});
+  // resetCounters();
+
+  // buffer.pop();
+
+  // REQUIRE(NonDefaultConstructible::nOfDtorCalls == 1);
+}
+
+TEST_CASE("Lvalue push copies element", "[Non-Default-Constructible Element Suite]") {
+  // resetCounters();
+  // BoundedBuffer<NonDefaultConstructible> buffer{5};
+  // NonDefaultConstructible element{23};
+
+  // buffer.push(element);
+
+  // REQUIRE(NonDefaultConstructible::nOfCopyConstructions == 1);
+}
+
+TEST_CASE("Rvalue push moves element", "[Non-Default-Constructible Element Suite]") {
+  // resetCounters();
+  // BoundedBuffer<NonDefaultConstructible> buffer{5};
+  // NonDefaultConstructible element{23};
+
+  // buffer.push(std::move(element));
+
+  // REQUIRE(NonDefaultConstructible::nOfMoveConstructions == 1);
+}
+
+TEST_CASE("Lvalue push must not copy assign", "[Non-Default-Constructible Element Suite]") {
+  // BoundedBuffer<NonDefaultConstructible> buffer{5};
+  // NonDefaultConstructible element{23};
+
+  // resetCounters();
+  // buffer.push(element);
+
+  // REQUIRE(NonDefaultConstructible::nOfCopyAssignments == 0);
+}
+
+TEST_CASE("Rvalue push must not move assign", "[Non-Default-Constructible Element Suite]") {
+  // BoundedBuffer<NonDefaultConstructible> buffer{5};
+  // NonDefaultConstructible element{23};
+
+  // resetCounters();
+  // buffer.push(std::move(element));
+
+  // REQUIRE(NonDefaultConstructible::nOfMoveAssignments == 0);
+}
---
+++ week05/exercise_templates/01_bounded_buffer_with_iterators/tests/catch2/BoundedBufferSemanticSuite.cpp
@@ -0,0 +1,147 @@
+
+#include "BoundedBuffer.hpp"
+
+#include "MemoryOperationCounter.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <utility>
+
+TEST_CASE("push(lvalue) copies element", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1};
+   // MemoryOperationCounter counter{}, expected{0, 1, true};
+   // buffer.push(counter);
+   // REQUIRE(expected == buffer.front());
+}
+
+TEST_CASE("push(rvalue) moves element", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1};
+   // MemoryOperationCounter counter{}, expected{1, 0, true};
+   // buffer.push(std::move(counter));
+   // REQUIRE(expected == buffer.front());
+}
+
+TEST_CASE("Element in BoundedBuffer is copied along copy constructor", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1};
+   // MemoryOperationCounter counter{}, expected{0, 2, true};
+   // buffer.push(counter);
+   // BoundedBuffer<MemoryOperationCounter> const &constBuffer = buffer;
+   // BoundedBuffer<MemoryOperationCounter> bufferCopied{constBuffer};
+   // REQUIRE(expected == bufferCopied.front());
+}
+
+TEST_CASE("Element in BoundedBuffer is copied along copy assignment operator", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1}, bufferCopied{1};
+   // MemoryOperationCounter counter{}, expected{0, 2, true};
+   // buffer.push(counter);
+   // BoundedBuffer<MemoryOperationCounter> const &constBuffer = buffer;
+   // bufferCopied = constBuffer;
+   // REQUIRE(expected == bufferCopied.front());
+}
+
+TEST_CASE("Element in BoundedBuffer is moved along move constructor", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1};
+   // MemoryOperationCounter counter{}, expected{1, 0, true};
+   // buffer.push(std::move(counter));
+   // BoundedBuffer<MemoryOperationCounter> bufferCopied{std::move(buffer)};
+   // REQUIRE(expected == bufferCopied.front());
+}
+
+TEST_CASE("Element in BoundedBuffer is moved along move assignment operator", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1}, bufferMoved{1};
+   // MemoryOperationCounter counter{}, expected{1, 0, true};
+   // buffer.push(std::move(counter));
+   // bufferMoved = std::move(buffer);
+   // REQUIRE(expected == bufferMoved.front());
+}
+
+TEST_CASE("Element in BoundedBuffer is copied along copy assignment operator in multiple copies", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1}, intermediate1{1}, intermediate2{1}, bufferCopied{1};
+   // MemoryOperationCounter counter{}, expected{0, 4, true};
+   // buffer.push(counter);
+   // bufferCopied = intermediate2 = intermediate1 = buffer;
+   // REQUIRE(expected == bufferCopied.front());
+}
+
+TEST_CASE("Element in BoundedBuffer is moved along move assignment operator in multiple moves", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1}, intermediate1{1}, intermediate2{1}, bufferMoved{1};
+   // MemoryOperationCounter counter{}, expected{1, 0, true};
+   // buffer.push(std::move(counter));
+   // bufferMoved = std::move(intermediate1 = std::move(intermediate2 = std::move(buffer)));
+   // REQUIRE(expected == bufferMoved.front());
+}
+
+TEST_CASE("BoundedBuffer copied is returned from assignment", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1}, bufferCopied{1};
+   // MemoryOperationCounter counter{}, expected{0, 2, true};
+   // buffer.push(counter);
+   // BoundedBuffer<MemoryOperationCounter> const &constBuffer = buffer;
+   // REQUIRE(expected == (bufferCopied = constBuffer).front());
+}
+
+TEST_CASE("BoundedBuffer moved is returned from assignment", "[Semantic Suite]") {
+   // BoundedBuffer<MemoryOperationCounter> buffer{1}, bufferMoved{1};
+   // MemoryOperationCounter counter{}, expected{1, 0, true};
+   // buffer.push(std::move(counter));
+   // REQUIRE(expected == (bufferMoved = std::move(buffer)).front());
+}
+
+
+TEST_CASE("Capacity is copied in Constructor", "[Semantic Suite]") {
+//   // given
+//   BoundedBuffer<MemoryOperationCounter> buffer{4};
+//   buffer.push(MemoryOperationCounter{});
+//   buffer.push(MemoryOperationCounter{});
+
+//   // when
+//   BoundedBuffer<MemoryOperationCounter> copy{buffer};
+//   copy.push(MemoryOperationCounter{});
+//   copy.push(MemoryOperationCounter{});
+
+//   // then
+//   REQUIRE(copy.full());
+}
+
+TEST_CASE("Capacity is copied in Assignment", "[Semantic Suite]") {
+//   // given
+//   BoundedBuffer<MemoryOperationCounter> buffer{4}, copy{1};
+//   buffer.push(MemoryOperationCounter{});
+//   buffer.push(MemoryOperationCounter{});
+
+//   // when
+//   copy = buffer;
+//   copy.push(MemoryOperationCounter{});
+//   copy.push(MemoryOperationCounter{});
+
+//   // then
+//   REQUIRE(copy.full());
+}
+
+TEST_CASE("Capacity is moved in Constructor", "[Semantic Suite]") {
+//   // given
+//   BoundedBuffer<MemoryOperationCounter> buffer{4};
+//   buffer.push(MemoryOperationCounter{});
+//   buffer.push(MemoryOperationCounter{});
+
+//   // when
+//   BoundedBuffer<MemoryOperationCounter> copy{std::move(buffer)};
+//   copy.push(MemoryOperationCounter{});
+//   copy.push(MemoryOperationCounter{});
+
+//   // then
+//   REQUIRE(copy.full());
+}
+
+TEST_CASE("Capacity is moved in Assignment", "[Semantic Suite]") {
+//   // given
+//   BoundedBuffer<MemoryOperationCounter> buffer{4}, copy{1};
+//   buffer.push(MemoryOperationCounter{});
+//   buffer.push(MemoryOperationCounter{});
+
+//   // when
+//   copy = std::move(buffer);
+//   copy.push(MemoryOperationCounter{});
+//   copy.push(MemoryOperationCounter{});
+
+//   // then
+//   REQUIRE(copy.full());
+}
