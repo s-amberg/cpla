2353b899bbff95a729607c32344bcd985d9b6642
tcorbat <thomas.corbat@gmail.com>
2024-04-07 19:36:27 +0000

    Migrate w07 to Catch2

---
+++ week07/exercise_solutions/02_compile_time_fibonacci/tests/FibonacciBenchmark.cpp
@@ -0,0 +1,32 @@
+
+#include "Fibonacci.hpp"
+#include "catch2/catch_message.hpp"
+
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+
+#include <format>
+#include <utility>
+
+//--------- 1d) Benchmark ---------
+
+TEST_CASE("Fibonacci calculation", "[Fibonacci Benchmark Suite]") {
+  auto [input, expected] = GENERATE(table<unsigned long long, unsigned long long>({
+    {0, 0}, 
+    {1, 1}, 
+    {2, 1}, 
+    {3, 2}, 
+    {4, 3}, 
+    {5, 5}, 
+    // {46, 1836311903} // Runs long!
+    }));
+  auto benchmarkName = std::format("Benchmark fibo({})", input);
+  BENCHMARK(std::move(benchmarkName)) {
+    return fibo(input);
+  };
+  CAPTURE(input, expected);
+  REQUIRE(fibo(input) == expected);
+}
---
+++ week07/exercise_templates/03_vector_literal_type/tests/Constants.hpp
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+
+constexpr double sqrt_14 = 3.7416573867739413855837487323165; // = std::sqrt(14.0); // std::sqrt(14.0) is not a constant expression until C++26
+
+
+#endif
