82a3ec0f323953280652dcd69eacb20486fbb3ed
Felix Morgner <felix.morgner@ost.ch>
2024-06-06 00:43:10 +0200

    week14: add CMake example

---
+++ week14/lecture_examples/04_emscripten_cmake/include/lib.hpp
@@ -0,0 +1,8 @@
+#ifndef EMSCRIPTEN_CMAKE_LIB_HPP
+#define EMSCRIPTEN_CMAKE_LIB_HPP
+
+#include <string>
+
+auto get_hello(std::string name) -> std::string;
+
+#endif
---
+++ week14/lecture_examples/04_emscripten_cmake/src/main.cpp
@@ -0,0 +1,7 @@
+#include "lib.hpp"
+
+#include <iostream>
+
+auto main() -> int {
+  std::cout << get_hello("CplA") << '\n';
+}
